c1bc197186baa5f082b2e5b2e1cfc2888ce5661f
Kendrick Elifritz <kendrickelifritz@gmail.com>
2024-03-19 17:09:23 -0700

    Use HashRouter for gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+
 import Header from './components/Header'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Contact from './pages/Contact/Contact'
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   )
 }
 
